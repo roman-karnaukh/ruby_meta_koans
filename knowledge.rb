4df0e58f0cd3fc40483ee4acef4f1d11691a83d7
roman-karnaukh <roman.karnaukh@gmail.com>
2016-04-19 15:28:59 +0300

    refactoring

--- knowledge.rb
+++ knowledge.rb
@@ -3,7 +3,7 @@
      atr, value = atr.first if atr.is_a? Hash
 
      define_method "#{atr}" do
-       unless instance_variable_defined?("@#{atr}")
+       unless instance_variable_defined? "@#{atr}"
          instance_variable_set("@#{atr}", value) if value
          instance_variable_set("@#{atr}", instance_eval(&block)) if block
        end
@@ -12,9 +12,5 @@
 
      define_method("#{atr}=") {|v| instance_variable_set("@#{atr}", v)}
 
-     define_method "#{atr}?" do
-       return false if instance_variable_get("@#{atr}").nil?
-       instance_variable_defined?("@#{atr}")
-     end
+     define_method("#{atr}?") { !send(atr).nil? }
    end
-
